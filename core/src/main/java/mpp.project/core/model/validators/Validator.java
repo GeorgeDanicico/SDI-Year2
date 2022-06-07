fb35d686c4008398dd83f39aa3a4fc7f6d326fc6
George-Iulian Danicico <72121511+GeorgeDanicico@users.noreply.github.com>
2022-06-07 20:48:54 +0300

    Delete core directory

--- core/src/main/java/mpp.project/core/model/validators/Validator.java
+++
@@ -1,8 +0,0 @@
-package mpp.project.core.model.validators;
-
-
-import mpp.project.core.exceptions.ValidatorException;
-
-public interface Validator<T> {
-    void validate(T entity) throws ValidatorException;
-}
--- core/src/main/java/mpp.project/core/repository/CustomRepositorySupport.java
+++
@@ -1,9 +0,0 @@
-package mpp.project.core.repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-public class CustomRepositorySupport {
-    @PersistenceContext
-    private EntityManager entityManager;
-}
