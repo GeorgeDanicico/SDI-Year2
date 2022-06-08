7ba199dbfd9e7fe90dac462628218b400599aeb0
George-Iulian Danicico <72121511+GeorgeDanicico@users.noreply.github.com>
2022-06-08 14:02:10 +0300

    Delete Movieapp directory

--- Movieapp/movieapp-core/src/main/java/ro/ubb/movieapp/core/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.ubb.movieapp.core.repository;
-
-import ro.ubb.movieapp.core.model.Movie;
-
-/**
- * author: radu
- */
-public interface MovieRepository extends MovieAppRepository<Movie, Long> {
-}
